91b1626c54f57366c2a40f91647f625119b85799
Kwak <brettkwak@gmail.com>
2025-03-02 21:52:59 +0900

    feat : Send message on startup & stop

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+# Store chat ID
+CHAT_ID = os.getenv("CHAT_ID")
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -18,10 +21,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-
+async def send_startup_message(application):
+    await application.bot.send_message(chat_id=CHAT_ID, text="Bot is now Online")
+async def send_stop_message(application):
+    await application.bot.send_message(chat_id=CHAT_ID, text="Bot is now Offline")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    application = (ApplicationBuilder().token(os.getenv("BOT_TOKEN"))
+                   .post_init(send_startup_message)
+                   .post_stop(send_stop_message)
+                   .build())
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)

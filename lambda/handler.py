c49b64bbe342f51fb154c4e6552c5068544c7733
BKwak28 <brettkwak@gmail.com>
2026-03-04 04:16:21 +0900

    feat : Add Telegram alert function

--- lambda/handler.py
+++ lambda/handler.py
@@ -2,6 +2,8 @@ import os
 import io
 import boto3
 import pandas as pd
+import requests
+from datetime import datetime, timezone, timedelta
 
 from data_io import json_to_df
 from determine_cross import check_signal
@@ -29,3 +31,27 @@ def write_csv_to_s3(df: pd.DataFrame):
     df.to_csv(buf, index=False)
     s3.put_object(Bucket=S3_BUCKET, Key=DATA_KEY, Body=buf.getvalue())
     print("Successfully saved updated CSV to S3.")
+
+# Telegram Environment Variables
+BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+# Send Telegram alert
+def send_telegram_alert(message: str):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("Telegram credentials missing")
+        return
+
+    et_time = datetime.now(timezone.utc) - timedelta(hours=5)
+    kst_time = datetime.now(timezone.utc) + timedelta(hours=9)
+    text = f"{message}\n🕐 {et_time.strftime('%H:%M')} ET\n🕐 {kst_time.strftime('%H:%M')} KST"
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text}
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        print(f"Telegram alert sent successfully.")
+    except Exception as e:
+        print(f"Failed to send Telegram message: {e}")

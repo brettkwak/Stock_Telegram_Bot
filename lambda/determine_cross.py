9f3070b68294da04d331fe833f0347da39890f79
BKwak28 <brettkwak@gmail.com>
2026-03-04 02:56:58 +0900

    feat : Add base cross detection logic

---
+++ lambda/determine_cross.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+def check_signal(df):
+
+    df = df.sort_values(by='Date', ascending=False).reset_index(drop=True)
+
+    # Set MA value
+    short_MA = 3
+    long_MA = 160
+
+    # Pre-Calculate MA
+    df['Short_MA'] = df['Close'].rolling(window=short_MA).mean()
+    df['Long_MA'] = df['Close'].rolling(window=long_MA).mean()
+
+    # Get Today's MA
+    current_short_MA = round(df['Short_MA'].iloc[short_MA - 1], 2)
+    current_long_MA = round(df['Long_MA'].iloc[long_MA - 1], 2)
+    print(f"{short_MA} MA : {current_short_MA}")
+    print(f"{long_MA} MA : {current_long_MA}")
+
+    # Get Yesterday's MA
+    prev_short_MA = round(df['Short_MA'].iloc[short_MA], 2)
+    prev_long_MA = round(df['Long_MA'].iloc[long_MA], 2)
+    print(f"prev {short_MA} MA : {prev_short_MA}")
+    print(f"prev {long_MA} MA : {prev_long_MA}")
+
+    # Cross Check
+    if current_short_MA > current_long_MA and prev_short_MA < prev_long_MA:
+        return "✅Golden Cross✅"
+    elif current_short_MA < current_long_MA and prev_short_MA > prev_long_MA:
+        return "⛔Death Cross⛔"
+    else:
+        return "No Cross"
